0b0b0e1e6a2339cadae2104e40e157887fffa8a6
ruyuanzhang <ruyuanzhang@gmail.com>
2018-06-21 13:37:48 -0500

    test

--- imageprocessutils/makeimagestack3dfiles.py
+++ imageprocessutils/makeimagestack3dfiles.py
@@ -59,10 +59,10 @@ def makeimagestack3dfiles(m, outputprefix=None, skips=[5, 5, 5], k=[0, 0, 0], \
         outputprefix = os.getcwd()  # the current directory
         outputprefix = outputprefix + os.sep
         _is_writeimage = False
-    (folerpath, header) = os.path.split(outputprefix)
+    folderpath, header = os.path.split(outputprefix)
 
     # create the folder if not exist.
-    assert system.makedirs(folerpath)
+    assert system.makedirs(folderpath+os.sep)  # note that we should add a os.sep to create the folder
 
     # define permutes
     imglist = []
@@ -84,9 +84,9 @@ def makeimagestack3dfiles(m, outputprefix=None, skips=[5, 5, 5], k=[0, 0, 0], \
         f = imageprocess.makeimagestack(temp, **kwargs)
         imglist.append(f)
         # write the image
-        fname = '%s/%s_view%d.png' % (folerpath, header, dim)
+        fname = '%s/%s_view%d.png' % (folderpath, header, dim)
         # note that plt.imsave can automatically recognize the vmin and vmax, no
         # need to convert it to uint8 like in matlab
-        if is_writeimage:  # if not, only return the images of three views
+        if _is_writeimage:  # if not, only return the images of three views
             imsave(fname, f, cmap=cmap)
     return imglist  # reverse list to keep compatible the original axis
